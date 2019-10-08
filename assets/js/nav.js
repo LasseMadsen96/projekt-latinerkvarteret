aa6c86493a9a3df2cfd2f85aef1d8565f7a2e8ec
Gittyhubhub <carinavessel@yahoo.dk>
2019-10-08 11:13:25 +0200

    Update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -8,12 +8,13 @@ const flipCardContainer = document.getElementsByClassName("flip-card__navigation
 window.onload = function() {
     // paa det foerste element tilfoejer vi et event, som er af type click, naar man klikker paa knappen skal den udfoerer den specificerede function.
     mobileNavBtn[0].addEventListener("click", toggleActiveClass);
+    
 }
 
 function toggleActiveClass(){
+    
     // i en variabel ser vi om containeren har en klasse der hedder active.
     const checkClass = flipCardContainer[0].classList.contains("active");
-
     if(checkClass){
         // hvis den har en active klasse skal den fjerne den fra elementet (containeren)
         flipCardContainer[0].classList.remove("active");
